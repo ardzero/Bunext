4a4fa48013a49f6717030de9d6cccf4fd5d1c069
darkid <farhanchowdhury6543@gmail.com>
2024-12-24 18:40:47 +0600

    drive redirect

--- src/components/utils/featureFlag.tsx
+++ src/components/utils/featureFlag.tsx
@@ -1,24 +1,24 @@
 import {
-	type FeatureFlagName,
-	canViewFeature,
+  type FeatureFlagName,
+  canViewFeature,
 } from "@/lib/config/featureflags";
 import { getUser } from "@/lib/config/user";
 import type { ReactNode } from "react";
 
 export function FeatureFlag({
-	featureFlag,
-	children,
+  featureFlag,
+  children,
 }: {
-	featureFlag: FeatureFlagName | FeatureFlagName[];
-	children: ReactNode;
+  featureFlag: FeatureFlagName | FeatureFlagName[];
+  children: ReactNode;
 }) {
-	const user = getUser();
+  const user = getUser();
 
-	if (Array.isArray(featureFlag)) {
-		return featureFlag.every((flag) => canViewFeature(flag, user))
-			? children
-			: null;
-	}
+  if (Array.isArray(featureFlag)) {
+    return featureFlag.every((flag) => canViewFeature(flag, user))
+      ? children
+      : null;
+  }
 
-	return canViewFeature(featureFlag, user) ? children : null;
+  return canViewFeature(featureFlag, user) ? children : null;
 }
